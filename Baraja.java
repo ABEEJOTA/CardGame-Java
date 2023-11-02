7083c51c17f898d189768d45e622c2ec8d41bfd5
Adrià <148556255+ABEEJOTA@users.noreply.github.com>
2023-11-02 16:36:54 +0100

    Basic game finished
    Added player number selection and significant number of fixes and additions for a better user experience.

---
+++ Baraja.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.juegocartasbasico;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author pl2023266
+ */
+public class Baraja {
+    private ArrayList<Carta> m_cartas;
+    
+    Baraja(ArrayList<Carta> cartas){
+        m_cartas = cartas;
+    }
+    
+    public ArrayList<Carta> getCartas(){
+        return m_cartas;
+    }
+    
+    public void setCartas(ArrayList<Carta> cartas){
+        m_cartas = cartas;
+    }
+    
+    public Carta getCartaBaraja(int i){
+        return m_cartas.get(i);
+    }
+    
+    public void borrarCartaBaraja(int i){
+        m_cartas.remove(i);
+    }
+}
